da18953d82116df534b1738e01437cfa75958b81
a-thread <aiden.threadgoode@gmail.com>
2020-09-10 12:54:39 -0400

    seed file to put initial content in table

---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department(dept_name) VALUES("accounting"), ("sales"), ("human resources");
+INSERT INTO roles(title, salary, dept_id) VALUES("accountant", 80000, 1), ("sales person", 40000, 2), ("hr manager", 60000, 3);
+INSERT INTO employee(first_name, last_name, role_id) VALUES("Corey", "Yates", 1), ("Laura", "Croft", 3), ("Terminator", "Nolastname", 2);
