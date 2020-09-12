b62a59fff550f3081805c02dde9652141b90f143
a-thread <aiden.threadgoode@gmail.com>
2020-09-12 00:45:00 -0400

    all functions working

--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,50 @@
-const connection = require("../config/config");
+const connection = require("../connection/connection");
 
 class Db {
     constructor() {
         this.connection = connection;
     }
-    
+
     findAll() {
-        return this.connection.query("SELECT * FROM employee INNER JOIN department ON (employee.role_id = department.id) LEFT JOIN role ON (employee.manager_id = role.id)")
+        return this.connection.query(`SELECT employee.id AS ID, CONCAT( employee.first_name, " ", employee.last_name ) AS Employee, title AS Title, salary AS Salary, dept_name AS Department, CONCAT( m.first_name, " ", m.last_name ) AS Manager
+        FROM employee
+        LEFT JOIN roles ON employee.role_id = roles.id
+        LEFT JOIN department ON roles.dept_id = department.id
+        LEFT JOIN employee m ON m.id = employee.manager_id`)
     }
 
     findDept() {
-        return this.connection.query("SELECT * FROM department")
+        return this.connection.query(`SELECT id AS ID, dept_name AS Department FROM department`)
     }
-    
-    findMang() {
-        return this.connection.query("SELECT * FROM department")
+
+    getRoles() {
+        return this.connection.query(`SELECT roles.id AS ID, roles.title AS Title, roles.salary AS Salary, department.dept_name AS "Department"
+        FROM roles
+        LEFT JOIN department ON roles.dept_id = department.id`)
+    }
+
+    getEmp() {
+        return this.connection.query(`SELECT employee.id, employee.first_name, employee.last_name FROM employee`)
+    }
+
+    addEmp(res) {
+        if (res.mangId === -1) {
+            return this.connection.query('INSERT INTO employee SET ?', { first_name: res.firstName, last_name: res.lastName, role_id: res.roleId });
+        } else {
+            return this.connection.query('INSERT INTO employee SET ?', { first_name: res.firstName, last_name: res.lastName, role_id: res.roleId, manager_id: res.mangId });
+        }
+    }
+
+    addDept(res) {
+        return this.connection.query('INSERT INTO department SET ?', { dept_name: res.name });
+    }
+
+    addRole(res) {
+        return this.connection.query('INSERT INTO roles SET ?', { title: res.name, salary: res.salary });
+    }
+
+    delete(table, id) {
+        return this.connection.query(`DELETE FROM ?? WHERE id = ?`, [ table, id ]);
     }
 }
 
