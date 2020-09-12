b4cbe1d1247d0a0fc8213e09bc1c795995b80a78
a-thread <aiden.threadgoode@gmail.com>
2020-09-12 00:43:56 -0400

    added more employees to seed file

--- src/sql/seeds.sql
+++ src/sql/seeds.sql
@@ -1,3 +1,25 @@
-INSERT INTO department(dept_name) VALUES("accounting"), ("sales"), ("human resources");
-INSERT INTO roles(title, salary, dept_id) VALUES("accountant", 80000, 1), ("sales person", 40000, 2), ("hr manager", 60000, 3);
-INSERT INTO employee(first_name, last_name, role_id) VALUES("Corey", "Yates", 1), ("Laura", "Croft", 3), ("Terminator", "Nolastname", 2);
+INSERT INTO
+    department(dept_name)
+VALUES
+("accounting"),
+    ("sales"),
+    ("human resources");
+
+
+INSERT INTO
+    roles(title, salary, dept_id)
+VALUES
+("accountant", 80000, 1),
+    ("sales person", 40000, 2),
+    ("hr manager", 60000, 3);
+
+
+INSERT INTO
+    employee(first_name, last_name, role_id, manager_id)
+VALUES
+("Magumi", "Amatsuka", 1, Null),
+    ("Makoto", "Ariga", 3, Null),
+    ("Rachel", "Bighead", 2, Null),
+    ("Candy", "Caramilla", 1, 1),
+    ("Yawara", "Chatora", 3, 2),
+    ("Ida", "Davis", 2, 3);
